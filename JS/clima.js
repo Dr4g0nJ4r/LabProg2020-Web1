ef17cdaaa760a2135c6f020d1b0f9fa7975a00f7
Ulises <ulises.jaramillo@est.fi.uncoma.edu.ar>
2020-10-18 22:49:11 -0300

    creacion de la funcion llamadasHistorico

--- JS/clima.js
+++ JS/clima.js
@@ -19,6 +19,16 @@ function tiempoUnix() {
         
 }
 
+function llamadasHistorico(){
+    //se realizan 5 llamadas a la API historico, uno por cada día antes de la fecha actual.
+
+    fetch("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+this.actual.coord.lat+"&lon="+this.actual.coord.lon+"&dt="+this.tiempo+"&units=metric&appid=073b5617fc4dbf48ce277078f57f3caf")//historico
+            .then(Response => Response.json())
+            .then(data => {console.log(data);
+                setHistorico(data);})
+
+}
+
 console.log("tiempo actual = " + Math.floor(Date.now() / 1000))
 console.log("tiempo 5 días atras = " + (Math.floor(Date.now() / 1000) - (86400 *5)))
 
@@ -31,15 +41,12 @@ function obtenerDatos(ciudad){
     .then(Response => Response.json())
     .then(data => {console.log(data);
         setActual(data);
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+actual.coord.lat+"&lon="+actual.coord.lon+"&exclude=current,minutely,hourly,alerts&units=metric&appid=073b5617fc4dbf48ce277078f57f3caf")// pronostico
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+this.actual.coord.lat+"&lon="+this.actual.coord.lon+"&exclude=current,minutely,hourly,alerts&units=metric&appid=073b5617fc4dbf48ce277078f57f3caf")// pronostico
         .then(Response => Response.json())
         .then(data => {console.log(data);
-            setPronostico(datos);
+            setPronostico(data);
             tiempoUnix();//actualiza el tiempo a 5 días atras de la fecha actual
-            fetch("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+actual.coord.lat+"&lon="+actual.coord.lon+"&dt="+tiempo+"&units=metric&appid=073b5617fc4dbf48ce277078f57f3caf")//historico
-            .then(Response => Response.json())
-            .then(data => {console.log(data);
-                setHistorico(data);})}
+            llamadasHistorico();}
             );
         })
     .catch(err => console.log(err))
