90ce12641cc0f6b6e5f4cfd8a1323f788c1a08e2
Ulises <ulises.jaramillo@est.fi.uncoma.edu.ar>
2020-10-19 01:23:19 -0300

    correcciones

--- JS/clima.js
+++ JS/clima.js
@@ -2,7 +2,7 @@
 //obtenidos desde una API. tendremos  tres grupos de datos.
 //los datos del historico, actuales y el pronostico.
 
-let historico={}; // 5 días antes...
+var historico = []; // 5 días antes...
 let actual // datos actuales del clima
 let pronostico // datos 5 días después..
 
@@ -12,16 +12,14 @@ function tiempoUnix(t) {
         
 }
 
-function llamadasHistorico(h){
+function llamadasHistorico(){
     //se realizan 5 llamadas a la API historico, uno por cada día antes de la fecha actual.
     for(i=1;i<=5;i++){
-        
         fetch("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+this.actual.coord.lat+"&lon="+this.actual.coord.lon+"&dt="+tiempoUnix(i)+"&units=metric&appid=073b5617fc4dbf48ce277078f57f3caf")//historico
         .then(Response => Response.json())
         .then(data => {console.log(data);
-            h.i=data})
+            this.historico.push(data)})
     }
-    setHistorico(h);
 
 }
 
@@ -43,7 +41,7 @@ function obtenerDatos(ciudad){
         .then(Response => Response.json())
         .then(data => {console.log(data);
             setPronostico(data);
-            llamadasHistorico(this.historico);}
+            llamadasHistorico();}
             );
         })
     .catch(err => console.log(err))
