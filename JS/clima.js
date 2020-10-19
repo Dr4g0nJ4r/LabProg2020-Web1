af8fff1311e681f07605ed704fb0ace8221d88aa
Ulises <ulises.jaramillo@est.fi.uncoma.edu.ar>
2020-10-18 21:26:00 -0300

    cración del metodo unixtime

--- JS/clima.js
+++ JS/clima.js
@@ -5,6 +5,7 @@
 let historico; // 5 días antes...
 let actual // datos actuales del clima
 let pronostico // datos 5 días después..
+let tiempo //tiempo Unix de los 5 días anteriores al actual
 
 /*
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Neuquén&units=metric&appid=073b5617fc4dbf48ce277078f57f3caf")
@@ -13,9 +14,12 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Neuquén&units=metric&a
     .catch(err => console.log(err))
 */
 function tiempoUnix() {
-    //método por el cual se calcula el tiempo Unix (tiempo medido en segundos) de los 5 días previos al acual...    
+    //método por el cual se calcula el tiempo Unix (tiempo medido en segundos) de los 5 días previos al acual...
+        
 }
 
+console.log(Date.now())
+/*
 function obtenerDatos(ciudad){
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+ciudad+"&units=metric&appid=073b5617fc4dbf48ce277078f57f3caf")
     .then(Response => Response.json())
@@ -26,7 +30,8 @@ function obtenerDatos(ciudad){
         .then(Response => Response.json())
         .then(data => {console.log(data);
             setPronostico(datos);
-            fetch("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+data.coord.lat+"&lon="+data.coord.lon+"&dt={time}&appid=073b5617fc4dbf48ce277078f57f3caf")
+            tiempoUnix();
+            fetch("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat="+data.coord.lat+"&lon="+data.coord.lon+"&dt="+tiempo+"&appid=073b5617fc4dbf48ce277078f57f3caf")
             .then(Response => Response.json())
             .then(data => {console.log(data);
                 setHistorico(data);})}
@@ -35,7 +40,7 @@ function obtenerDatos(ciudad){
     .catch(err => console.log(err))
 
 }
-
+*/
 function setActual(datos){
     this.actual=datos;
 }
