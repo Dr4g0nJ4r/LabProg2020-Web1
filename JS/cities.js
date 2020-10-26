7e4d1d041f92233cfe3099b029ffa7dc8af6f299
Ulises <ulises.jaramillo@est.fi.uncoma.edu.ar>
2020-10-26 20:08:20 -0300

    comentarios

--- JS/cities.js
+++ JS/cities.js
@@ -1,18 +1,4 @@
-/*import * as data from './listCities.json';
-const { cities } = data;
-console.log(cities);
-*/
-/*
-const cities = [];
-fetch('./JS/listCities.json')
-    .then(response => response.json())
-    //.then(obj => console.log(obj))
-    //.then(data => cities.push(...data))
-    //.then(blob => console.log(blob))
-    //.then(blob => blob.json())
-    .then(data => console.log(data))
-    .then(data => cities.push(...data))
-*/
+
 var cities = [];
 getCities()
 function getCities() {
